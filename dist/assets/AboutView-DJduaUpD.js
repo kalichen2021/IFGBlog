7741d42ffb1ca92b905707e7a9a5525f9623456a
kalichen2021 <kalichen2021@outlook.com>
2024-10-02 20:35:29 +0800

    wf

---
+++ dist/assets/AboutView-DJduaUpD.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-C778kxUA.js";import{c as t,a as s,o as a}from"./vendor-Cla9jNWU.js";const r={},n={class:"about"};function c(i,e){return a(),t("div",n,e[0]||(e[0]=[s("h1",null,"This is an about page",-1)]))}const l=o(r,[["render",c]]);export{l as default};
